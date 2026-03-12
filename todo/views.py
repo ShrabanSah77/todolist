cad4c2d3d1624987cef6736e4609621242df2ab1
ShrabanSah77 <shrabanshah77@gmail.com>
2026-03-11 21:59:04 -0400

    making todo app

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
+
+def index(request):
+    tasks = Task.objects.all().order_by("-created")
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+        
