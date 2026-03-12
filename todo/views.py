0e67b9ad6337aaa5a859340c76eab877811aeabe
ShrabanSah77 <shrabanshah77@gmail.com>
2026-03-11 23:05:58 -0400

    creating my to-do list

--- todo/views.py
+++ todo/views.py
@@ -2,15 +2,40 @@ from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 
-# Create your views here.
+# showing existing list
 
 def index(request):
-    tasks = Task.objects.all().order_by("-created")
+    tasks = Task.objects.all().order_by('-created')
     form = TaskForm()
 
-    if request.method == "POST":
+    if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect("/")
-        
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'index.html', context)
+
+# updating existing list
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'update.html', context)
+
+#deleting the list which is not required
+
+def deleteTask(request, pk):
+    task = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+    context = {'task': task}
+    return render(request, 'delete.html', context)
